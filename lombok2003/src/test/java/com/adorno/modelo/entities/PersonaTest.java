05fa7c701262de1db7847676514766f22fc63f74
programacion WEB <jasanchezm01@iesarroyoharnina.es>
2026-01-21 10:49:40 +0100

    apirest primeros endpoints

--- lombok2003/src/test/java/com/adorno/modelo/entities/PersonaTest.java
+++ lombok2003/src/test/java/com/adorno/modelo/entities/PersonaTest.java
@@ -4,11 +4,10 @@ package com.adorno.modelo.entities;
 import org.junit.jupiter.api.Test;
 
 class PersonaTest {
-
+	
 	@Test
 	void test() {
-		Persona persona=new Persona();
-		
+		Persona persona=new Persona();		
 	}
 
 }
